1495ad0c2417c9e9612f44abcfee41cc2c43885f
JasonHuang <lohas821019@gmail.com>
2022-03-28 18:28:11 +0800

    servo.ino revise

---
+++ arduino-python.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 15:39:44 2022
+
+@author: Jason
+"""
+import serial
+import time
+
+COM_PORT = 'COM3'
+baudRate = 9600
+ser = serial.Serial(COM_PORT, baudRate, timeout=0.5)
+
+try:
+    while True:
+        # 接收用戶的輸入值並轉成小寫
+        choice = input('按1、按2、按e關閉程式  ').lower()
+
+        if choice == '1':
+            
+            print('傳送1指令')
+            ser.write(b'LED_ON\n')  # 訊息必須是位元組類型
+            time.sleep(0.5)
+            
+        elif choice == '2':
+            print('傳送2指令')
+            ser.write(b'LED_OFF\n')
+            time.sleep(0.5)
+            
+        elif choice == 'e':
+            ser.close()
+            sys.exit()
+        else:
+            print('錯誤…')
+
+        while ser.in_waiting:
+            mcu_feedback = ser.readline().decode()  # 接收回應訊息並解碼
+            print('控制板回應：', mcu_feedback)
+            
+except KeyboardInterrupt:
+    ser.close()
+    print('再見！')
--- servo/servo.ino
+++ servo/servo.ino
@@ -7,6 +7,8 @@
 Servo RCL;
 Servo RCR;
 
+int DIR;
+
 void setup()
 {
     Serial.begin(115200); //
@@ -21,60 +23,76 @@ void setup()
 
 void loop()
 {
-    
+ moto(DIR);  
 }
 
-//void moto(byte DIR, int PWMA, int PWMB)
-//{
-//    switch (DIR)
-//    {
-//    case 0:
-//        RC.write(70,70);
-//        break;
-//    case 1:
-//        RC.write(0,30);
-//        break;
-//    case 2:
-//        RC.write(0,50);
-//        break;
-//    case 3:
-//        RC.write(0,90);
-//        break;
-//    case 4:
-//        RC.write(30,0);
-//        break;
-//    case 5:
-//        RC.write(50,50);
-//        break;
-//    case 6:
-//        RC.write(50,130); // 往左自轉
-//        break;
-//    case 7:
-//        RC.write(0,0);
-//        break;
-//    case 8:
-//        RC.write(50,0);
-//        break;
-//    case 9:
-//        RC.write(130,50); // 往右自轉
-//        break;
-//    case 10:
-//        RC.write(50,50);
-//        break;
-//    case 11:
-//        RC.write(0,0);
-//        break;
-//    case 12:
-//        RC.write(90,0);
-//        break;
-//    case 13:
-//        RC.write(0,0);
-//        break;
-//    case 15:
-//        RC.write(50,50);
-//        break;
-//    case 16:
-//        RC.write(130,130); //後退
-//        break;
-//    }
-//}
+void moto(byte DIR)
+{
+    switch (DIR)
+    {
+    case 0:
+        RCL.write(70);
+        RCR.write(70);
+        break;
+    case 1:
+        RCL.write(0);
+        RCR.write(30);
+        break;
+    case 2:
+        RCL.write(0);
+        RCR.write(50);
+        break;
+    case 3:
+        RCL.write(0);
+        RCR.write(90);
+        break;
+    case 4:
+        RCL.write(30);
+        RCR.write(0);
+        break;
+    case 5:
+        RCL.write(50);
+        RCR.write(50);
+        break;
+    case 6:
+        RCL.write(50);
+        RCR.write(130);; // 往左自轉
+        break;
+    case 7:
+        RCL.write(0);
+        RCR.write(0);
+        break;
+    case 8:
+        RCL.write(50);
+        RCR.write(0);
+        break;
+    case 9:
+        RCL.write(130);
+        RCR.write(50);; // 往右自轉
+        break;
+    case 10:
+        RCL.write(50);
+        RCR.write(50);;
+        break;
+    case 11:
+        RCL.write(0);
+        RCR.write(0);;
+        break;
+    case 12:
+        RCL.write(90);
+        RCR.write(0);;
+        break;
+    case 13:
+        RCL.write(0);
+        RCR.write(0);;
+        break;
+    case 15:
+        RCL.write(50);
+        RCR.write(50);;
+        break;
+    case 16:
+        RCL.write(130);
+        RCR.write(130);; //後退
+        break;
+    }
+}
