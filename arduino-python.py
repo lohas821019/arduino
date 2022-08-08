535673a2952841315d3d55f3260a088342cc8710
JasonHuang <lohas821019@gmail.com>
2022-08-08 16:49:37 +0800

    修正錯誤問題

--- arduino-python.py
+++ arduino-python.py
@@ -6,37 +6,34 @@ Created on Mon Mar 28 15:39:44 2022
 """
 import serial
 import time
+import numpy as np
 
-COM_PORT = 'COM3'
-baudRate = 115200
+COM_PORT = 'COM4'
+baudRate = 9600
 ser = serial.Serial(COM_PORT, baudRate, timeout=0.5)
 
-try:
-    while True:
-        # 接收用戶的輸入值並轉成小寫
-        choice = input('請輸入模式 0 ~ 16\n').lower()
-        
-        if choice == 'q':
-            break
 
-        elif (int(choice) < 17):
-            # print(int(choice))
-            choice = np.array(choice).tobytes()
-            ser.write(choice)            # 訊息必須是位元組類型
-            ser.flush()
-            time.sleep(0.5)
+choice = input('請輸入模式 0 ~ 16\n').lower()
 
-        while ser.in_waiting:
-            mcu_feedback = ser.readline()
-            mcu_feedback = mcu_feedback.decode("utf-8") # 接收回應訊息並解碼
-            mcu_feedback = mcu_feedback.strip()
-            print('控制板回應：', mcu_feedback)
-            time.sleep(0.1)
-
-        # while ser.in_waiting:
-        #     sys.stdout.write(ser.readline())
-        #     sys.stdout.flush()
-
-finally:
+if choice == 'q':
     ser.close()
-    print('再見！')
+
+elif (int(choice) < 17):
+    # print(int(choice))
+    choice = np.array(choice).tobytes()
+    ser.write(choice)            # 訊息必須是位元組類型
+    ser.flush()
+    time.sleep(0.5)
+
+while ser.in_waiting:
+    mcu_feedback = ser.readline()
+    mcu_feedback = mcu_feedback.decode("utf-8") # 接收回應訊息並解碼
+    mcu_feedback = mcu_feedback.strip()
+    print('控制板回應：', mcu_feedback)
+    time.sleep(0.1)
+
+# while ser.in_waiting:
+#     sys.stdout.write(ser.readline())
+#     sys.stdout.flush()
+
+
